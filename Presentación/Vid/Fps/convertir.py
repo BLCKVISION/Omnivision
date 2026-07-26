294e01603db083bf351279c2a003412b482d623d
Albert Fonseca <blckvisions@gmail.com>
2026-07-26 01:22:00 -0400

    Eliminar carpeta Presentación no requerida

--- Presentación/Vid/Fps/convertir.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import os
-
-for f in os.listdir('.'):
-    if f.endswith('.png'):
-        with Image.open(f) as img:
-            img.convert('RGB').save(f.replace('.png', '.jpg'), quality=95)
-        os.remove(f)
